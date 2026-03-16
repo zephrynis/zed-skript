a4830cbe948705bdae422b8630e640103b662121
Zephrynis <zephrynis.yt@gmail.com>
2026-03-16 00:03:57 +0000

    Fix language registration: Update ABI to 14, fix query directory structure, and update repo URLs

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 15
+#define LANGUAGE_VERSION 14
 #define STATE_COUNT 250
 #define LARGE_STATE_COUNT 97
 #define SYMBOL_COUNT 247
@@ -5043,7 +5043,7 @@ static bool ts_lex_keywords(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0},
   [1] = {.lex_state = 140},
   [2] = {.lex_state = 140},
@@ -17408,7 +17408,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_skript(void) {
     .state_count = STATE_COUNT,
     .large_state_count = LARGE_STATE_COUNT,
     .production_id_count = PRODUCTION_ID_COUNT,
-    .supertype_count = SUPERTYPE_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
     .parse_table = &ts_parse_table[0][0],
@@ -17428,13 +17427,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_skript(void) {
     .keyword_lex_fn = ts_lex_keywords,
     .keyword_capture_token = sym_word_token,
     .primary_state_ids = ts_primary_state_ids,
-    .name = "skript",
-    .max_reserved_word_set_size = 0,
-    .metadata = {
-      .major_version = 0,
-      .minor_version = 1,
-      .patch_version = 0,
-    },
   };
   return &language;
 }
